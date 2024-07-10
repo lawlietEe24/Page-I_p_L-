2a851c3bd830f465749cc93f579efa1548655069
lawlietEe24 <estebanjarrin188@gmail.com>
2024-07-10 16:37:53 -0500

    implementacion de las pestañas

---
+++ java/main.js
@@ -0,0 +1,13 @@
+const btnToggle = document.querySelector('.toggle-btn');
+
+btnToggle.addEventListener('click', function(){
+    document.querySelector('.aside').classList.toggle('active');
+});
+
+const btnToggle_main = document.querySelector('.toggle-btn');
+
+btnToggle_main.addEventListener('click', function(){
+    document.querySelector('.main').classList.toggle('mainActive');
+});
+
+// xd
